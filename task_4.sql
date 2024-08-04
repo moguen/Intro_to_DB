79ebbccba277e15bea7c3fb2b2aa7a79cbaf5859
smail <pelsm91@gmail.com>
2024-08-05 00:20:27 +0100

    Add task_4.sql to print full description of the books tablee

--- task_4.sql
+++ task_4.sql
@@ -1,12 +1,3 @@
--- Query to get the table structure from information_schema
-SELECT 
-    COLUMN_NAME AS 'Column Name',
-    COLUMN_TYPE AS 'Data Type',
-    IS_NULLABLE AS 'Nullable',
-    COLUMN_KEY AS 'Key',
-    EXTRA AS 'Extra Information'
-FROM 
-    information_schema.COLUMNS
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store'
-    AND TABLE_NAME = 'books';
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
